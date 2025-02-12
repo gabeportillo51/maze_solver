4d16ecc354e220708017ce6d0155b0e122f764af
gabeportillo51 <gaporti51@gmail.com>
2025-02-12 13:53:16 -0700

    L5 Complete

--- draw.py
+++ draw.py
@@ -10,3 +10,29 @@ class Line:
 
     def draw(self, canvas, fill_color):
         canvas.create_line(self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, canvas):
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = canvas
+        self.top_wall = True
+        self.bottom_wall = True
+        self.left_wall = True
+        self.right_wall = True
+
+    def draw(self, point_1, point_2):
+        self._x1 = point_1.x
+        self._y1 = point_1.y
+        self._x2 = point_2.x
+        self._y2 = point_2.y
+        if self.top_wall == True:
+            Line(Point(self._x1, self._y1), Point(self._x2, self._y1)).draw(self._win, "black")
+        if self.bottom_wall == True:
+            Line(Point(self._x1, self._y2), Point(self._x2, self._y2)).draw(self._win, "black")
+        if self.left_wall == True:
+            Line(Point(self._x1, self._y1), Point(self._x1, self._y2)).draw(self._win, "black")
+        if self.right_wall == True:
+            Line(Point(self._x2, self._y2), Point(self._x2, self._y1)).draw(self._win, "black")
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from window import Window
-from draw import Point, Line
+from draw import Point, Line, Cell
 
 win = Window(800, 600)
-point_1 = Point(55, 200)
-point_2 = Point(500, 13)
-point_3 = Point(99, 450)
-point_4 = Point(250, 350)
-point_5 = Point(700, 100)
-line_1 = Line(point_1, point_5)
-line_2 = Line(point_2, point_4)
-line_3 = Line(point_3, point_1)
-win.draw_line(line_1, "black")
-win.draw_line(line_2, "red")
-win.draw_line(line_3, "green")
+first_cell = Cell(win.canvas)
+second_cell = Cell(win.canvas)
+third_cell = Cell(win.canvas)
+fourth_cell = Cell(win.canvas)
+fifth_cell = Cell(win.canvas)
+first_cell.draw(Point(10, 10), Point(90, 90))
+second_cell.top_wall = False
+second_cell.draw(Point(100, 100), Point(160, 160))
+third_cell.bottom_wall = False
+third_cell.draw(Point(200, 200), Point(270, 270))
+fourth_cell.left_wall = False
+fourth_cell.draw(Point(300, 300), Point(360, 360))
+fifth_cell.right_wall = False
+fifth_cell.draw(Point(450, 450), Point(550, 550))
 win.wait_for_close()
