0b97ad3574844869c0fcda7e3aade632a24ad5a0
gabeportillo51 <gaporti51@gmail.com>
2025-02-12 12:30:37 -0700

    L4 Complete

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from window import Window
+from draw import Point, Line
 
 win = Window(800, 600)
+point_1 = Point(55, 200)
+point_2 = Point(500, 13)
+point_3 = Point(99, 450)
+point_4 = Point(250, 350)
+point_5 = Point(700, 100)
+line_1 = Line(point_1, point_5)
+line_2 = Line(point_2, point_4)
+line_3 = Line(point_3, point_1)
+win.draw_line(line_1, "black")
+win.draw_line(line_2, "red")
+win.draw_line(line_3, "green")
 win.wait_for_close()
--- window.py
+++ window.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from draw import Line
 
 class Window:
     
@@ -21,3 +22,7 @@ class Window:
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
