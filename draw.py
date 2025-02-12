0f278ce96cf2561357d10856a4829163096fdfca
gabeportillo51 <gaporti51@gmail.com>
2025-02-12 15:17:48 -0700

    L7 Complete

--- draw.py
+++ draw.py
@@ -1,3 +1,39 @@
+import time
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = []
+        for i in range(self.num_cols):
+            self._cells.append([Cell(self.win.canvas) for i in range(self.num_rows)])
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self._draw_cell(i,j)
+        
+        
+    def _draw_cell(self, i, j):
+        moving_x = self.x1 + (i*self.cell_size_x)
+        moving_y = self.y1 + (j*self.cell_size_y)
+        upper_left = Point(moving_x, moving_y)
+        lower_right = Point(moving_x + self.cell_size_x, moving_y + self.cell_size_y)
+        self._cells[i][j].draw(upper_left, lower_right)
+        self._animate()
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
+
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 from window import Window
-from draw import Point, Line, Cell
+from draw import Point, Line, Cell, Maze
 
 win = Window(800, 600)
-first_cell = Cell(win.canvas)
-second_cell = Cell(win.canvas)
-third_cell = Cell(win.canvas)
-fourth_cell = Cell(win.canvas)
-fifth_cell = Cell(win.canvas)
-first_cell.draw(Point(236, 10), Point(45, 471))
-second_cell.draw(Point(18, 173), Point(66, 160))
-third_cell.draw(Point(84, 212), Point(270, 270))
-fourth_cell.draw(Point(342, 75), Point(360, 360))
-fifth_cell.draw(Point(745, 323), Point(656, 156))
-first_cell.draw_move(second_cell)
-first_cell.draw_move(third_cell)
-first_cell.draw_move(fourth_cell, undo=True)
-first_cell.draw_move(fifth_cell)
+maze = Maze(2, 2, 20, 20, 50, 50, win)
+maze._create_cells
 win.wait_for_close()
